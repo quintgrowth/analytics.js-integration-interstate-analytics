c8546b2d0adbd9ec490b3727b5a4764d323ca124
Piotr Kozłowski <piotr.kozlowski@gmail.com>
2016-02-11 03:30:16 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# analytics.js-integration-interstate-analytics [![Build Status][ci-badge]][ci-link]
+
+Interstate Analytics integration for [Analytics.js][].
+
+## License
+
+Released under the [MIT license](License.md).
+
+
+[Analytics.js]: https://segment.com/docs/libraries/analytics.js/
+<!--[ci-link]: https://circleci.com/gh/segment-integrations/analytics.js-integration-customerio-->
+<!--[ci-badge]: https://circleci.com/gh/segment-integrations/analytics.js-integration-customerio.svg?style=svg-->
---
+++ test/index.test.js
@@ -0,0 +1,150 @@
+'use strict';
+
+var Analytics = require('analytics.js-core').constructor;
+var integration = require('analytics.js-integration');
+var sandbox = require('clear-env');
+var tester = require('analytics.js-integration-tester');
+var Interstate = require('../lib/');
+
+describe('Interstate Analytics', function() {
+  var analytics;
+  var interstate;
+  var options = {
+    siteId: 'abccbaabccbaabccbaabccbaabccbaabccbaabcd'
+  };
+
+  beforeEach(function() {
+    analytics = new Analytics();
+    interstate = new Interstate(options);
+    analytics.use(Interstate);
+    analytics.use(tester);
+    analytics.add(interstate);
+  });
+
+  afterEach(function() {
+    analytics.restore();
+    analytics.reset();
+    interstate.reset();
+    sandbox();
+  });
+
+  it('should have the right settings', function() {
+    analytics.compare(Interstate, integration('Interstate Analytics')
+      .global('interstate')
+      .option('siteId', ''));
+  });
+
+  describe('before loading', function() {
+    beforeEach(function() {
+      analytics.stub(interstate, 'load');
+    });
+
+    describe('#initialize', function() {
+      it('should create the window.interstate object', function() {
+        analytics.assert(!window.interstate);
+        analytics.initialize();
+        analytics.assert(window.interstate);
+      });
+
+      it('should call #load', function() {
+        analytics.initialize();
+        analytics.called(interstate.load);
+      });
+    });
+  });
+
+  describe('loading', function() {
+    it('should load', function(done) {
+      analytics.load(interstate, done);
+      done();
+    });
+  });
+
+  describe('after loading', function() {
+    beforeEach(function(done) {
+      analytics.once('ready', done);
+      analytics.initialize();
+    });
+
+    describe('#identify', function() {
+      it('should send visit just after initialize', function() {
+        var log = window.interstate.getLogHistory()[0];
+        analytics.assert(log.metadata.event_name === 'visit');
+        analytics.assert(log.metadata.project_key === options.siteId);
+        analytics.assert(log.metadata.version_date === '2015-06-09');
+      });
+
+      it('should send an alias', function() {
+        analytics.identify('somemail@interstateanalytics.com');
+        var logs = window.interstate.getLogHistory();
+        analytics.assert(logs.length === 2);
+
+        var visitLog = logs[0];
+        analytics.assert(visitLog.metadata.event_name === 'visit');
+        analytics.assert(visitLog.metadata.project_key === options.siteId);
+        analytics.assert(visitLog.metadata.version_date === '2015-06-09');
+
+        var aliasLog = logs[1];
+        analytics.assert(aliasLog.metadata.event_name === 'alias_user');
+        analytics.assert(aliasLog.metadata.aliased_name === 'somemail@interstateanalytics.com');
+        analytics.assert(aliasLog.metadata.version_date === '2015-06-09');
+
+        analytics.assert(visitLog.metadata.user_identifier === aliasLog.metadata.user_identifier);
+      });
+
+      it('should not send if there is no userId()', function() {
+        analytics.identify({ trait: true });
+        var logs = window.interstate.getLogHistory();
+        analytics.assert(logs.length === 1);
+
+        var visitLog = logs[0];
+        analytics.assert(visitLog.metadata.event_name === 'visit');
+        analytics.assert(visitLog.metadata.project_key === options.siteId);
+        analytics.assert(visitLog.metadata.version_date === '2015-06-09');
+      });
+    });
+
+    describe('#track', function() {
+      it('should send a signup conversion', function() {
+        analytics.track('signup');
+
+        var logs = window.interstate.getLogHistory();
+        analytics.assert(logs.length === 2);
+
+        var visitLog = logs[0];
+        analytics.assert(visitLog.metadata.event_name === 'visit');
+        analytics.assert(visitLog.metadata.project_key === options.siteId);
+        analytics.assert(visitLog.metadata.version_date === '2015-06-09');
+
+        var conversionLog = logs[1];
+        analytics.assert(conversionLog.metadata.event_name === 'conversion');
+        analytics.assert(conversionLog.metadata.conversion_name === 'signup');
+        analytics.assert(conversionLog.metadata.version_date === '2015-06-09');
+        analytics.assert(!conversionLog.metadata.order_number);
+        analytics.assert(!conversionLog.metadata.price);
+
+        analytics.assert(visitLog.metadata.user_identifier === conversionLog.metadata.user_identifier);
+      });
+
+      it('should send a purchase conversion with properties', function() {
+        analytics.track('purchase', { revenue: 31.95, order_number: 'abc123' });
+        var logs = window.interstate.getLogHistory();
+        analytics.assert(logs.length === 2);
+
+        var visitLog = logs[0];
+        analytics.assert(visitLog.metadata.event_name === 'visit');
+        analytics.assert(visitLog.metadata.project_key === options.siteId);
+        analytics.assert(visitLog.metadata.version_date === '2015-06-09');
+
+        var conversionLog = logs[1];
+        analytics.assert(conversionLog.metadata.event_name === 'conversion');
+        analytics.assert(conversionLog.metadata.conversion_name === 'purchase');
+        analytics.assert(conversionLog.metadata.version_date === '2015-06-09');
+        analytics.assert(conversionLog.metadata.order_number === 'abc123');
+        analytics.assert(conversionLog.metadata.price === 31.95);
+
+        analytics.assert(visitLog.metadata.user_identifier === conversionLog.metadata.user_identifier);
+      });
+    });
+  });
+});
